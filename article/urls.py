18aa21d18df18eb099349349ac1e1a70d6141cb0
harrylyx <lyxvae@gmail.com>
2016-05-17 18:17:31 +0800

    change

--- article/urls.py
+++ article/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', article_views.index, name='index'),
-    url(r'^(?P<id>\d+)/$', 'views.detail', name='detail'),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
 ]
