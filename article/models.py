24eaeb5a4de6ecbb1e2cff2e2e158cc1b996b58a
harrylyx <lyxvae@gmail.com>
2016-05-19 16:12:15 +0800

    change models

--- article/models.py
+++ article/models.py
@@ -7,7 +7,9 @@ class Article(models.Model) :
     title = models.CharField(max_length = 100)  
     category = models.CharField(max_length = 50, blank = True)  
     date_time = models.DateTimeField(auto_now_add = True)
+    shortContent = models.TextField(blank = True, null = True)
     content = models.TextField(blank = True, null = True)
+
 
     def __unicode__(self) :
         return self.title
