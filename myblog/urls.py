f4bdf32c36d4a4f6f45027b217ff272fc94058aa
harrylyx <lyxvae@gmail.com>
2016-05-19 18:37:52 +0800

    add search

--- myblog/urls.py
+++ myblog/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^archives/$', 'article.views.archives', name = 'archives'),
     url(r'^aboutme/$', 'article.views.about_me', name = 'about_me'),
+    url(r'^tag(?P<tag>\w+)/$', 'article.views.search_tag', name = 'search_tag'),
+    url(r'^search/$','article.views.blog_search', name = 'search'),
 
 ]
