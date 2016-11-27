f6dabccaca7e7da9be13667b93568f53bea41350
harrylyx <lyxvae@gmail.com>
2016-11-27 13:30:12 +0800

    wtf

--- article/views.py
+++ article/views.py
@@ -17,6 +17,7 @@ def detail(request, id):
     try:
         post = Article.objects.get(id=str(id))
         post.content = markdown.markdown(post.content, extensions=['markdown.extensions.extra', 'markdown.extensions.codehilite'])
+        '''
         last_view = request.session.get('last_view')#获取最后一次浏览本站的时间last_view
         if last_view:
             last_visit_time = datetime.datetime.strptime(last_view[:-7], "%Y-%m-%d %H:%M:%S")
@@ -27,6 +28,7 @@ def detail(request, id):
             post.views += 1
             post.save()
         request.session['last_view'] = str(datetime.datetime.now())#更新session
+	'''
     except Article.DoesNotExist:
         raise Http404
     return render(request, 'post.html', {'post': post})
